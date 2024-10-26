2aab6c2a968ec0f4ce35a912fcb5b256653e3282
Dushimepaulette1 <p.dushime1@alustudent.com>
2024-10-26 17:52:57 +0000

    updates

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
-"""Script that fetch 10 hot post for a given subreddit."""
+"""Script that fetches the top 10 hot posts for a given subreddit."""
+
 import requests
 
 
 def top_ten(subreddit):
-    """Return number of subscribers if @subreddit is valid subreddit.
-    if not return 0."""
-
+    """Prints the titles of the top 10 hot posts for a given subreddit.
+    If the subreddit is invalid, prints 'None'.
+    """
     headers = {'User-Agent': 'MyAPI/0.0.1'}
-    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
-    response = requests.get(subreddit_url, headers=headers)
-
-    if response.status_code == 200:
-        json_data = response.json()
-        for i in range(10):
-            print(
-                json_data.get('data')
-                .get('children')[i]
-                .get('data')
-                .get('title')
-            )
-    else:
-        print("OK")
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            posts = response.json().get('data', {}).get('children', [])
+            if posts:
+                for post in posts:
+                    print(post.get('data', {}).get('title'))
+            else:
+                print("None")
+        else:
+            print("None")
+    except requests.RequestException:
+        print("None")
