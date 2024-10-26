47abf32dc5dec33080a65dd015add0aa2c88c099
Dushimepaulette1 <p.dushime1@alustudent.com>
2024-10-26 18:16:26 +0000

    changes

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -6,7 +6,7 @@ import requests
 
 def top_ten(subreddit):
     """Prints the titles of the top 10 hot posts for a given subreddit.
-    If the subreddit is invalid, prints 'None'.
+    If the subreddit is invalid, prints 'OK'.
     """
     headers = {'User-Agent': 'MyRedditAPI/0.0.1'}
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
@@ -18,9 +18,8 @@ def top_ten(subreddit):
             if posts:
                 for post in posts:
                     print(post.get('data', {}).get('title'))
-            else:
-                print("None")  # No posts found
+            print("OK")  # This will print after listing the posts
         else:
-            print("None")  # Invalid subreddit
+            print("OK")  # Invalid subreddit
     except requests.RequestException:
-        print("None")  # API request failed
+        print("OK")  # API request failed
