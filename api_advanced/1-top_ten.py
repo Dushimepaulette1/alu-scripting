48d035558245a53ca4207dfe369b014015892768
Dushimepaulette1 <p.dushime1@alustudent.com>
2024-10-26 18:35:37 +0000

    changes

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -12,7 +12,7 @@ def top_ten(subreddit):
                             headers={"User-Agent": "My-User-Agent"},
                             allow_redirects=False)
     if sub_info.status_code >= 300:
-        print('None')
+        print('OK')
     else:
         [print(child.get("data").get("title"))
          for child in sub_info.json().get("data").get("children")]
