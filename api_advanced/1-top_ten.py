d66c6bd3162ca9412ec7780e07fbc99bd90e7f38
Dushimepaulette1 <p.dushime1@alustudent.com>
2024-10-26 09:17:54 +0000

    api advanced

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,31 +1,24 @@
 #!/usr/bin/python3
-"""Fetch and display the top 10 hot posts of a given subreddit."""
+"""Script that fetch 10 hot post for a given subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts listed for a given subreddit.
-    
-    Args:
-        subreddit (str): The name of the subreddit to query.
-        
-    Returns:
-        None
-    """
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    """Return number of subscribers if @subreddit is valid subreddit.
+    if not return 0."""
 
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        # Check for successful status code and valid JSON response
-        if response.status_code == 200:
-            json_data = response.json().get('data', {}).get('children', [])
-            if json_data:
-                for post in json_data:
-                    print(post.get('data', {}).get('title'))
-            else:
-                print("OK")
-        else:
-            print("None")
-    except (requests.RequestException, ValueError):
-        print("None")
+    headers = {'User-Agent': 'MyAPI/0.0.1'}
+    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
+    response = requests.get(subreddit_url, headers=headers)
+
+    if response.status_code == 200:
+        json_data = response.json()
+        for i in range(10):
+            print(
+                json_data.get('data')
+                .get('children')[i]
+                .get('data')
+                .get('title')
+            )
+    else:
+        print(None)
