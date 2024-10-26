d6bf5e68d1b8603641fe4f96dc37873c4cbbbed1
Dushimepaulette1 <p.dushime1@alustudent.com>
2024-10-26 09:25:23 +0000

    api advanced

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
-"""Script that fetch 10 hot post for a given subreddit."""
+"""Script that fetches the titles of the first 10 hot posts for a given subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """Return number of subscribers if @subreddit is valid subreddit.
-    if not return 0."""
-
+    """Prints the titles of the first 10 hot posts for a given subreddit.
+    
+    If the subreddit does not exist, print None.
+    """
     headers = {'User-Agent': 'MyAPI/0.0.1'}
-    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
-    response = requests.get(subreddit_url, headers=headers)
+    subreddit_url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    response = requests.get(subreddit_url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
-        json_data = response.json()
-        for i in range(10):
-            print(
-                json_data.get('data')
-                .get('children')[i]
-                .get('data')
-                .get('title')
-            )
+        json_data = response.json().get('data', {}).get('children', [])
+        if json_data:
+            for post in json_data:
+                print(post.get('data', {}).get('title'))
+            print("OK")  # Print "OK" after printing the titles if the subreddit exists
+        else:
+            print("OK")  # Print "OK" if no posts are available
     else:
-        print(None)
+        print("None")  # Print "None" if the subreddit does not exist
