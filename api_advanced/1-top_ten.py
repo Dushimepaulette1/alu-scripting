027a1df290a2e9f16e131f5aa5752688c895f0ad
Dushimepaulette1 <p.dushime1@alustudent.com>
2024-10-26 18:34:11 +0000

    changes

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,28 +1,18 @@
 #!/usr/bin/python3
-"""Script that fetches the top 10 hot posts for a given subreddit."""
-
-import requests
-import sys
+"""Module for task 1"""
 
 
 def top_ten(subreddit):
-    """Prints the titles of the top 10 hot posts for a given subreddit.
-    If the subreddit is invalid, prints 'OK'.
-    """
-    headers = {'User-Agent': 'MyRedditAPI/0.0.1'}
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    
-    try:
-        response = requests.get(
-            url, headers=headers, allow_redirects=False
-        )
-        if response.status_code == 200:
-            posts = response.json().get('data', {}).get('children', [])
-            if posts:
-                for post in posts:
-                    print(post.get('data', {}).get('title'))
-            sys.stdout.write("OK")  # Output "OK" without extra characters
-        else:
-            sys.stdout.write("OK")  # Output "OK" without extra characters
-    except requests.RequestException:
-        sys.stdout.write("OK")  # Output "OK" without extra characters
+    """Queries the Reddit API and returns the top 10 hot posts
+    of the subreddit"""
+    import requests
+
+    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
+        print('None')
+    else:
+        [print(child.get("data").get("title"))
+         for child in sub_info.json().get("data").get("children")]
